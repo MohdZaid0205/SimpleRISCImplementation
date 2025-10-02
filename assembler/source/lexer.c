d4844c7de06a5413e33d8d07e8dfd9b76ef15c1e
Mohd Zaid <mdzaid0205@gmail.com>
2025-10-02 13:20:12 +0530

    Refactor literal handling and improve documentation
    Revised the handling of literals by introducing modular collector
    and validator functions for strings, numbers, and comments.
    Added `LiteralCollectors` and `LiteralValidators` arrays to
    enforce a predefined order of processing. Removed the
    `LiteralBackTable` and shifted focus to collectors and validators.
    
    Improved the `__generic_collector` function to enhance the
    iteration logic for processing literals. Updated comments to
    provide detailed guidelines for maintaining order and ensuring
    correct implementation. These changes improve modularity,
    clarity, and maintainability of the code.

--- assembler/source/lexer.c
+++ assembler/source/lexer.c
@@ -1,7 +1,10 @@
 #include "lexer.h"
 
-// TODO: fix this for 0b and 0x, maybe introduce a manner to
-// TODO: handle numeric value, it always starts a numeric (assuming)
+//IMPORTANT: [>_] -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=+
+// * All following tables and definitions attached must be in pre-		|
+// * defined order as indexed in @TokenTypes <TOKEN_XX>.				|
+// * if altered with, this implementation will not work properly.		|
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-+
 
 const char* LiteralFrntTable[] = {
 	"\"",	// string literal starting with " symbol
@@ -11,7 +14,6 @@ const char* LiteralFrntTable[] = {
 	"#" ,	// comment start symbol, consumes line
 };
 
-
 const char* LiteralBackTable[] = {
 	"\"",	// ending of string literal ends with "
 	" " ,	// numeric terminator
@@ -20,11 +22,21 @@ const char* LiteralBackTable[] = {
 	"\n",	// comment is supposed to terminate on new line
 };
 
+//IDEA: [>_] -----------------------------------------------------------+
+// * Literal collector must be defined for all types of literals.		|
+// * Note that collector will start from its `frnt` value included and	|
+// * it must exit with its `back` value excluded, it is assumed	that	|
+// * literal does contain its `frnt` and `back` as result.				|
+// ---------------------------------------------------------------------+
+
+//NOTE: add more collectors implementations here, preferably.
 char* literal_strings_collector(FILE* fd);
 char* literal_decnums_collector(FILE* fd);
 char* literal_binnums_collector(FILE* fd);
 char* literal_hexnums_collector(FILE* fd);
 char* literal_comment_collector(FILE* fd);
+//NOTE: include all validator implementations in order below.
+//IMPORTANT: in `LiteralCollectors` inclusion must be orderd.
 
 char* (*LiteralCollectors[])(FILE*) = {
 	literal_strings_collector,
@@ -34,11 +46,22 @@ char* (*LiteralCollectors[])(FILE*) = {
 	literal_comment_collector,
 };
 
+//IDEA: [>_] -----------------------------------------------------------+
+// * Literal Validator is used to Validate all the parts that is		|
+// * present within previously collected literal, this may also be		|
+// * used for checking if collection is properly reasoned with.			|
+// ---------------------------------------------------------------------+
+
+
+//NOTE: add more validator implementations here, preferably.
 bool  literal_strings_validator(FILE* fd);
 bool  literal_decnums_validator(FILE* fd);
 bool  literal_binnums_validator(FILE* fd);
 bool  literal_hexnums_validator(FILE* fd);
 bool  literal_comment_validator(FILE* fd);
+
+//NOTE: include all validator implementations in order below.
+//IMPORTANT: in `LiteralValidators` inclusion must be orderd.
 
 bool (*LiteralValidators[])(FILE*) = {
 	literal_strings_validator,
@@ -64,12 +87,11 @@ char* __generic_collector(enum TokenTypes tt, FILE* fd, bool (*res)(char), bool 
 	//TODO: iterate thru frnt of the literal.
 
 	while ((collected = fgetc(fd) != EOF))
-{
+	{
 		if (all(collected))
 			__found++;
 		if (res(collected))
 			break;
-		__found++;
 	}
 
 	//TODO: iterate thru back of the literal.
