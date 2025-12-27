92fcb1880ab1c743fe2bd5c5e6074dbe8ba18763
Mohd Zaid <mdzaid0205@gmail.com>
2025-12-27 21:53:34 +0530

    Add ISA header and implementation files for arm32, i386, riscv32, and default ISA

---
+++ include/isa.h
@@ -0,0 +1,7 @@
+#ifndef ASSEMBLER_ISA_API_H
+#define ASSEMBLER_ISA_API_H
+
+extern const char* assembler_isa_name;
+extern const char* assembler_do_assemble_for_isa(const char* what);
+
+#endif
---
+++ src/isa/isa-arm32.c
@@ -0,0 +1,6 @@
+#include "isa.h"
+
+const char* assembler_isa_name = "arm32";
+const char* assembler_do_assemble_for_isa(const char* what){
+    return assembler_isa_name;
+}
